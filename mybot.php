36e6e81a04e372721cad9cbf1c28b848a897d477
farshadtehrani <35953869+farshadtehrani@users.noreply.github.com>
2018-01-30 14:42:54 +0330

    Add files via upload

--- mybot.php
+++ mybot.php
@@ -1,27 +1,35 @@
 <?php
-/**
-* telegram bot example.
-* https://github.com/farshadtehrani/farshadtest.git
-*/
-include("telegram.php");
-date_default_timezone_set("asia/tehran");
-//set the bot token
-$bot_id = "544210869:AAGdGFA18aY3PPGBWojblIfhRkPZuHOVUn8";
-$telegram = new telgram($bot_id);
-$result = $telegram->getdata();
-$text             = $telegram->text();
-$chat_id          = $telegram->chat_id();
-$username         = $telegram->username();
-$name             = $telegram->firstname();
-$family           = $telegram->lastname();
-$callback_data    = $telegram->callback_data();
-$callback_query   = $telegram->callback_query();
-$callback_chat_id = $telegram->callback_chat_id();
-$callback_id      = $telegram->callback_id();
-$message_id       = $telegram->message_id();
-$user_id          = $telegram->user_id();
 
-if(!is_null($text) && !is_null($chat_id)){
-	$content = array('chat_id' => $chat_id, 'text' => $user_id);
-	$telegram->sendReplyMessage($content);
+
+https://github.com/farshadtehrani/farshadtest.git
+
+
+header('Content-Type: text/html; charset=utf-8');
+$message= file_get_contents("php://input");
+$arrayMessage= json_decode($message, true);
+$token= "544210869:AAG6-Q6I1WZ_EH5FBeLpoYjkbTovw7-yS1g";
+$chat_id= $arrayMessage['message']['from']['id'];
+$command= $arrayMessage['message']['text'];
+$id = $arrayMessage['message']['message_id'];
+ 
+//Get Chat Id And Message Content
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+ 
+ 
+if($command == '/start'){
+ $text= "سلام، به ربات من خوش امدید";
+ $url= "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&amp;text=".$text."&amp;reply_markup=".$encodedMarkup;
+ file_get_contents($url);
+ 
+  
+  
+} else if($command == 'سلام'){
+ $text= "سلام خوبی چطوری";
+ $url= "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&amp;text=".$text."&amp;reply_markup=".$encodedMarkup;
+ file_get_contents($url);
+  
+ 
 }
+ ?>
